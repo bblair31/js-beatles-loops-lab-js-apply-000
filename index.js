4a7ba4a99a0f911add7d816417b9f25ea3ed96a6
Brennan Blair <jbb341@hotmail.com>
2018-08-29 17:19:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function theBeatlesPlay(musicians,instruments) {
   var play = []
-  for (var i = 0; i < musicians.length; i++)
-  re
+  for (var i = 0; i < musicians.length; i++) {
+   play.push(musicians[i] + instruments[i]) 
+  }
+  return play
 }
